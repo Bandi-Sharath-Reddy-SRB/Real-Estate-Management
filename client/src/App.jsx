a5e520db3351365a942e69b82943ab1917045950
Bandi Sharath Reddy <sharathbandi46@gmail.com>
2024-08-09 00:25:28 +0530

    Created Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 function App() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
