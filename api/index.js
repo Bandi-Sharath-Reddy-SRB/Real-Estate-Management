a6990bd1313f3e1fc924ecd0bb8f5f8748fbbb1a
Bandi Sharath Reddy <sharathbandi46@gmail.com>
2024-08-09 01:37:59 +0530

    Database Connected Successfully

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 // const express = require("express");
 
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  try {
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 const app = express();
 
 app.listen(3000, () => {
